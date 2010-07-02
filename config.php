12c7a4d065b7c88b802d5af8f241f7d91a80d518
iYorkie <iYorkie@infinityirc.com>
2010-07-02 16:27:58 +0100

    Config file for the Bot

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+$config['server'] = 'irc.server.here';
+$config['port'] = 'port.here';
+$config['nick'] = 'Nick';
+$config['ident'] = 'ident';
+$config['realname'] = 'Marge PHP Bot';
+$config['channels'] = '#chan1,#chan2';
+$config['password'] = 'nickservpassword';
+$config['prefix'] = '!';
+$config['controlchan'] = '#botcontrol';
+$config['quitmessage'] = 'I quit!';
